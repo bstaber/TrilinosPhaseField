e008f6522ccbbc01da6477347d58e9ac58c838ed
Brian <brian.staber@gmail.com>
2018-11-07 14:27:02 +0100

    Removed useless header

--- src/baseClassFEM.hpp
+++ src/baseClassFEM.hpp
@@ -15,8 +15,10 @@ namespace TPF {
 class baseClassFEM
 {
 public:
-    baseClassFEM();
-    ~baseClassFEM();
+    baseClassFEM(){
+    };
+    ~baseClassFEM(){
+    };
 
     mesh        * Mesh;
     Epetra_Comm * Comm;
@@ -26,8 +28,10 @@ public:
     Epetra_Import     * ImportToOverlapMap;
     Epetra_FECrsGraph * FEGraph;
 
-    virtual void setup_dirichlet_conditions();
-    virtual void apply_dirichlet_conditions(Epetra_FECrsMatrix & K, Epetra_FEVector & F, double & displacement);
+    virtual void setup_dirichlet_conditions(){
+    };
+    virtual void apply_dirichlet_conditions(Epetra_FECrsMatrix & K, Epetra_FEVector & F, double & displacement){
+    };
 };
 
 }
