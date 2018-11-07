e009c4086e059d36ccfa3a5314e223843a228f53
Brian <brian.staber@gmail.com>
2018-11-07 15:44:55 +0100

    Added elasticity problem

--- src/elasticity.hpp
+++ src/elasticity.hpp
@@ -5,12 +5,25 @@
 
 namespace TPF {
 
-  class elasticity: public baseClassFEM {
+  class elasticity : public baseClassFEM
+  {
+  public:
+      elasticity();
+      ~elasticity();
 
-    elasticity();
-    ~elasticity();
+      void create_FECrsGraph();
 
+      void aztecSolver(Epetra_FECrsMatrix & A, Epetra_FEVector & b, Epetra_Vector & u, Teuchos::ParameterList & paramList);
+
+      void assemblePureDirichlet_homogeneousForcing(Epetra_FECrsMatrix & K);
+      void stiffness_homogeneousForcing(Epetra_FECrsMatrix & K);
+
+      void compute_B_matrices(Epetra_SerialDenseMatrix & dx_shape_functions, Epetra_SerialDenseMatrix & B);
+
+      virtual void get_elasticity_tensor(unsigned int & e_lid, unsigned int & gp, Epetra_SerialDenseMatrix & tangent_matrix) = 0;
+
+      unsigned int n_bc_dof;
+      int * dof_on_boundary;
   };
-
 }
 #endif
