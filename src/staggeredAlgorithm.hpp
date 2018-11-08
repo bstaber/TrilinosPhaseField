cd09abc4832908f6c2dbe8ab0d5b50698a0f3b5e
Brian <brian.staber@gmail.com>
2018-11-08 10:47:45 +0100

    Setting up the staggered algorithm

---
+++ src/staggeredAlgorithm.hpp
@@ -0,0 +1,36 @@
+#ifndef STAGGEREDALGORITHM_HPP
+#define STAGGEREDALGORITHM_HPP
+
+#include "elasticity.hpp"
+#include "damage.hpp"
+
+namespace TPF {
+
+  class staggeredAlgorithm : public elasticity
+  {
+
+  public:
+
+    staggeredAlgorithm(Epetra_Comm & comm, mesh & mesh_);
+    ~staggeredAlgorithm();
+/*
+    void staggeredAlgorithmDirichletBC(Teuchos::ParameterList & ParametersList, bool print);
+
+    void computeDisplacement(Teuchos::ParameterList & ParameterList,
+                             Epetra_FECrsMatrix & matrix, Epetra_Vector & lhs, Epetra_FEVector & rhs,
+                             double & bc_disp);
+
+    void updateDamageHistory(Epetra_Vector & damageHistory,
+                             Epetra_Vector & displacement);
+*/
+    Teuchos::RCP<damage> phaseFieldBVP;
+    Teuchos::RCP<elasticity> elasticityBVP;
+
+    double gc, lc;
+    double E, nu, lambda, mu;
+
+    Epetra_Vector * damageSolutionOverlaped;
+
+  };
+}
+#endif
